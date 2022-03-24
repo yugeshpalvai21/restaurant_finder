18166904b01322140fb561f2418bdbeede718fc4
yugeshpalvai <yugeshpalvai21@gmail.com>
2022-03-24 20:00:14 +0530

    handle read and write restaurant data

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,9 +3,15 @@ class Restaurant
     @@file_path = name
   end
 
+  def self.file_path
+    @@file_path
+  end
+
   def self.file_exists?
+    return true if file_path && File.exists?(file_path)
   end
 
   def self.create_file
+    return true if file_path && File.open(file_path, 'w')
   end
 end
