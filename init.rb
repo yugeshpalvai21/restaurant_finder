bbf7d2b89460b368ad2bfb44adb45b0b676d2213
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-08-02 21:03:13 +0530

    cleans up initialize file

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 #This is starting point of programm
 
-# ruby maintails special variable called $: which maintains array of all lookup paths
+# ruby maintains special variable called $: which maintains array of all lookup paths
 # $:.each { |path| puts path }
 
 APP_ROOT = File.dirname(__FILE__)
@@ -9,5 +9,3 @@ require File.join(APP_ROOT, 'lib', 'guide')
 
 guide = Guide.new('restaurants.txt')
 guide.launch!
-
-
